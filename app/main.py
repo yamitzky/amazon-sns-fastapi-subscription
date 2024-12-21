022539ea7f257bbbeace0a46c0049660b0e8bd56
Mitsuki Ogasahara <negiga@gmail.com>
2024-12-22 00:39:21 +0900

    define request model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import logging
 
 from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
+
+from app.models import (
+    sns_message_adapter,
+)
 
 app = FastAPI()
 
@@ -12,5 +18,13 @@ logger = logging.getLogger(__name__)
 
 
 @app.post("/")
-async def sns_receiver(request: Request):
+async def sns_receiver(
+    request: Request,
+):
+    try:
+        # text/plain で送られてくるため、引数にはせず、json に変換
+        message = sns_message_adapter.validate_json(await request.body())
+    except ValidationError as e:
+        raise RequestValidationError(e.errors())
+
     return "ok"
---
+++ app/models.py
@@ -0,0 +1,43 @@
+from typing import Annotated, Literal, Optional
+
+from pydantic import BaseModel, ConfigDict, Field, HttpUrl, TypeAdapter
+from pydantic.alias_generators import to_pascal
+
+
+class SNSMessageBase(BaseModel):
+    type: Literal["Notification", "SubscriptionConfirmation", "UnsubscribeConfirmation"]
+    message_id: str
+    topic_arn: str
+    timestamp: str
+    signature_version: str
+    signature: str
+    signing_cert_url: Annotated[HttpUrl, Field(alias="SigningCertURL")]
+    message: str
+
+    model_config = ConfigDict(
+        alias_generator=to_pascal,
+        extra="allow",
+    )
+
+
+class SNSNotification(SNSMessageBase):
+    type: Literal["Notification"]
+    subject: Optional[str] = None
+
+
+class SNSSubscription(SNSMessageBase):
+    type: Literal["SubscriptionConfirmation"]
+    subscribe_url: Annotated[HttpUrl, Field(alias="SubscribeURL")]
+    token: str
+
+
+class SNSUnsubscribe(SNSMessageBase):
+    type: Literal["UnsubscribeConfirmation"]
+    subscribe_url: Annotated[HttpUrl, Field(alias="SubscribeURL")]
+    token: str
+
+
+SNSMessage = Annotated[
+    SNSNotification | SNSSubscription | SNSUnsubscribe, Field(discriminator="type")
+]
+sns_message_adapter: TypeAdapter[SNSMessage] = TypeAdapter(SNSMessage)
